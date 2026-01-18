4001b4fe79a10b31d29a34ad007d72eab8c9e671
dezeninaekaterina-creator <dezeninaekaterina@gmail.com>
2026-01-18 23:23:07 +1000

    Create print_info.py

---
+++ print_info.py
@@ -0,0 +1,9 @@
+def main():
+    full_name = "Деженина Екатерина Андреевна" 
+    group_number = "M1125-38.04.05фт" 
+    
+    print(f"ФИО: {full_name}")
+    print(f"Номер группы: {group_number}")
+
+if __name__ == "__main__":
+    main()
